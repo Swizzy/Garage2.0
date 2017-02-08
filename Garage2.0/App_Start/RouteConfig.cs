f23f4c6fe606fabb8b051686e65ee5f2d1b6460f
Swizzy <swizzy@xeupd.com>
2017-02-08 16:38:18 +0100

    Remove HomeController and make GarageController default

--- Garage2.0/App_Start/RouteConfig.cs
+++ Garage2.0/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Garage2._0
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Garage", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
