d9c453d13cbeb51c48dedc8ae200a0ee4243e237
Swizzy <swizzy@xeupd.com>
2017-02-08 16:26:30 +0100

    Add Garage controller

---
+++ Garage2.0/Controllers/GarageController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Garage2._0.DAL;
+using Garage2._0.Models;
+
+namespace Garage2._0.Controllers
+{
+    public class GarageController : Controller
+    {
+        private VehiclesContext db = new VehiclesContext();
+
+        // GET: Garage
+        public ActionResult Index()
+        {
+            return View(db.Vehicles.ToList());
+        }
+
+        // GET: Garage/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Vehicle vehicle = db.Vehicles.Find(id);
+            if (vehicle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicle);
+        }
+
+        // GET: Garage/Create
+        public ActionResult Checkin()
+        {
+            return View();
+        }
+
+        // POST: Garage/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Checkin([Bind(Include = "Id,Type,RegNumber,Color,Brand,Model,NumberOfWheels")] Vehicle vehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Vehicles.Add(vehicle);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(vehicle);
+        }
+
+        // GET: Garage/Delete/5
+        public ActionResult Checkout(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Vehicle vehicle = db.Vehicles.Find(id);
+            if (vehicle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicle);
+        }
+
+        // POST: Garage/Delete/5
+        [HttpPost, ActionName("Checkout")]
+        [ValidateAntiForgeryToken]
+        public ActionResult CheckoutConfirmed(int id)
+        {
+            Vehicle vehicle = db.Vehicles.Find(id);
+            db.Vehicles.Remove(vehicle);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
